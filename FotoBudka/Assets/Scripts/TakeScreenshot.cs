f5137bf54fe0080830015b0e0514fac4013eadce
PiotrSter <piotrster@o2.pl>
2022-03-13 19:24:00 +0100

    screenshot is done

--- FotoBudka/Assets/Scripts/TakeScreenshot.cs
+++ FotoBudka/Assets/Scripts/TakeScreenshot.cs
@@ -1,33 +1,47 @@
 using System;
+using System.IO;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TakeScreenshot : MonoBehaviour
 {
     DateTime dateTime;
     int width, height;
+    Canvas canvas;
 
     void Awake()
     {
         width = Screen.width;
         height = Screen.height;
+        canvas = FindObjectOfType<Canvas>();
     }
-    void TakeAScreenshot()
-    {
-        /*dateTime = DateTime.Now;
-        Texture2D screenshoot = new Texture2D(width, height);
-        Rect rect = new Rect(0, 0, width, height);
-        screenshoot.ReadPixels(rect, 0, 0);
-        screenshoot.Apply();
 
-        byte[] byteArray = screenshoot.EncodeToPNG();
-        System.IO.File.WriteAllBytes($"{Application.persistentDataPath}/Output/{dateTime.ToString()}", byteArray);*/
-        //ScreenCapture.CaptureScreenshot($"{Application.persistentDataPath}/Output/{dateTime.ToString()}");
+    IEnumerator TakeAScreenshot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        dateTime = DateTime.Now;
+        string path = $"{Application.persistentDataPath}/Output";
+        DirectoryInfo directoryInfo;
+        string fileName = dateTime.ToString("yyyy/MM/dd H-mm-ss");
+        //canvas.enabled = false;
+        if (!Directory.Exists(path))
+            directoryInfo = Directory.CreateDirectory(path);
+        if (!File.Exists($"{fileName}.png"))
+        {
+            Texture2D screenshoot = new Texture2D(width, height);
+            Rect rect = new Rect(0, 0, width, height);
+            screenshoot.ReadPixels(rect, 0, 0);
+            screenshoot.Apply();
+            byte[] byteArray = screenshoot.EncodeToPNG();
+            File.WriteAllBytes($"{path}/{fileName}.png", byteArray);
+        }
+        canvas.enabled = true;
     }
 
     public void PhotoButton()
     {
-        TakeAScreenshot();
+        canvas.enabled = false;
+        StartCoroutine(TakeAScreenshot());
     }
 }
